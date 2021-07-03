d758804480b4634d8a2f1bfd2a81854453cdbb3d
chilikinow <Sam9858853565>
2021-07-03 22:48:31 +0300

    1.3 change ProcessingUserMessage class, move all if blocks from Bot class

--- src/main/java/bot/ProcessingUserMessage.java
+++ src/main/java/bot/ProcessingUserMessage.java
@@ -1,86 +1,239 @@
 package bot;
 
+import commandSystem.InfoCommand;
+import commandSystem.ServiceInfoCommand;
+import org.apache.commons.io.FilenameUtils;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.*;
 
 public class ProcessingUserMessage {
 
-    public enum MessageType {
-        PROMO_MOBILE_TV_MENU,
-        PROMO_APPLIANCES_MENU,
-        DEVICE,
-        SYSTEM,
-        BONUS,
-        OTHER
-    }
-
-    public MessageType parseCategory(Message message){
+    public Object searchAnswer(Message message) {
 
         String messageText = message.getText();
 
-        //если сообщение содержит команду меню Акций Мобайл ТВ
-        var promoMobileTvSet = PromoInfo.getInstancePromoMobileTV().keySet();
-        Set<String> bufferPromoMobileTVSet = new HashSet<>(promoMobileTvSet);
-            bufferPromoMobileTVSet.add("/promo_mobile_tv");
-            bufferPromoMobileTVSet.add("Акции Мобайл ТВ");
-            bufferPromoMobileTVSet.add("Promo Mobile TV");
-        if (bufferPromoMobileTVSet.contains(messageText)){
-            return MessageType.PROMO_MOBILE_TV_MENU;
+        //При запросе Акции Мобайл ТВ
+
+        //формируется клавиатура с перечнем акций
+
+        if (messageText.equals("Акции Мобайл ТВ")
+                || messageText.equals("Promo Mobile TV")
+                || messageText.equals("/promo_mobile_tv")) {
+
+            var replyMessage = Response.createTextMessageWithKeyboard
+                    (message, "Список акций интернет магазина:\nhttps://galaxystore.ru/promo/",
+                            Response.TypeKeyboard.PROMO_MOBILE_TV);
+
+            return replyMessage;
         }
 
-        //если сообщение содержит команду меню акций БТ
-        var promoAppliancesSet = PromoInfo.getInstancePromoAppliances().keySet();
-        Set<String> bufferPromoAppliancesSet = new HashSet<>(promoAppliancesSet);
-            bufferPromoAppliancesSet.add("/promo_appliances");
-            bufferPromoAppliancesSet.add("Акции БТ");
-            bufferPromoAppliancesSet.add("Promo Appliances");
-        if (bufferPromoAppliancesSet.contains(messageText)){
-            return MessageType.PROMO_APPLIANCES_MENU;
-        }
+        //Поиск акций мобайл и тв
 
-        //если сообщение содержит название устройства
-        var categoryDeviceList = new Device().getCategoryDeviceList();
-        List<String> bufferCategoryDeviceList = new ArrayList<>(categoryDeviceList);
-            bufferCategoryDeviceList.add("/device_info");
-            bufferCategoryDeviceList.add("Характеристики устройств".toLowerCase(Locale.ROOT));
-            bufferCategoryDeviceList.add("Device info".toLowerCase(Locale.ROOT));
+        Map<String, String> promoMobileTVInfoMap = PromoInfo.getInstancePromoMobileTV();
 
-        String lowerMessageText = messageText.toLowerCase(Locale.ROOT);
+        if (promoMobileTVInfoMap.keySet().contains(messageText)) {
+            for (Map.Entry<String, String> entry : promoMobileTVInfoMap.entrySet()) {
+                if (messageText.equals(entry.getKey())) {
+                    StringBuilder replyText = new StringBuilder();
+                    replyText.append(entry.getKey() + "\n\n" + entry.getValue());
 
-        for (int i = 0; i < bufferCategoryDeviceList.size(); i++) {
-            if (lowerMessageText.startsWith(bufferCategoryDeviceList.get(i))){
-                return MessageType.DEVICE;
+                    //При запросе данной акции добавляется ссылка на таблицу с уточнением условий по каждой модели
+
+                    if ("Скидки по акции ценопад".equals(messageText)) {
+                        replyText.append("\n\nПодробности:\n\n");
+                        replyText.append(new BotData().getReadPromoInfoFileUrl());
+                    }
+
+                    var replyMessage = Response.createTextMessageWithKeyboard(message
+                            ,replyText.append("\n\n" + "/start_menu").toString()
+                            ,Response.TypeKeyboard.START);
+
+                    return replyMessage;
+                }
             }
         }
 
+        //При запросе Акции БТ
 
-        List<String> bufferSystemMessageList = new ArrayList<>();
-            bufferSystemMessageList.add(new BotData().getBotPassword());
-            bufferSystemMessageList.add("/start_menu");
-            bufferSystemMessageList.add("Сервис");
-            bufferSystemMessageList.add("Service");
-            bufferSystemMessageList.add("/service");
-            bufferSystemMessageList.add("Инфо Мобайл");
-            bufferSystemMessageList.add("/mobile_info");
-            bufferSystemMessageList.add("Инфо ТВ");
-            bufferSystemMessageList.add("/tv_info");
-            bufferSystemMessageList.add("Инфо БТ");
-            bufferSystemMessageList.add("/appliances_info");
-        if (bufferSystemMessageList.contains(messageText)){
-            return MessageType.SYSTEM;
+        //формируется клавиатура с перечнем акций
+
+        if (messageText.equals("Акции БТ")
+                || messageText.equals("Promo Appliances")
+                || messageText.equals("/promo_appliances")) {
+
+            var replyMessage = Response.createTextMessageWithKeyboard
+                    (message, "Список акций интернет магазина:\nhttps://galaxystore.ru/promo/",
+                            Response.TypeKeyboard.PROMO_APPLIANCES);
+
+            return replyMessage;
         }
 
-        List<String> bufferBonusMessageList = new ArrayList<>();
-        bufferBonusMessageList.add("Программа Лояльности");
-        bufferBonusMessageList.add("/bonus_card");
-        if (bufferBonusMessageList.contains(messageText)
-        || messageText.startsWith("9")
-        || messageText.startsWith("20")){
-            return MessageType.BONUS;
+        //поиск акций БТ
+
+        Map<String, String> promoAppliancesInfoMap = PromoInfo.getInstancePromoAppliances();
+
+        if (promoAppliancesInfoMap.keySet().contains(messageText)) {
+            for (Map.Entry<String, String> entry : promoAppliancesInfoMap.entrySet()) {
+                if (messageText.equals(entry.getKey())) {
+                    var replyMessage = Response.createTextMessageWithKeyboard(message
+                            , entry.getKey() + "\n\n" + entry.getValue() + "\n\n" + "/start_menu"
+                            , Response.TypeKeyboard.START);
+
+                    return replyMessage;
+                }
+            }
         }
 
-        return MessageType.OTHER;
+        //При запросе Характеристики устройств
+
+        //отправляется запрос модели
+
+        if (messageText.equals("Характеристики устройств")
+                || messageText.equals("Device info")
+                || messageText.equals("/device_info")) {
+            var replyMessage = Response.createTextMessage(message,
+                    "Введите название устройства:");
+
+            return replyMessage;
+        }
+
+        //поиск устройства
+
+        var categoryDeviceList = new Device().getCategoryDeviceList();
+        List<String> bufferCategoryDeviceList = new ArrayList<>(categoryDeviceList);
+
+        for (int i = 0; i < bufferCategoryDeviceList.size(); i++) {
+            if (messageText.toLowerCase(Locale.ROOT).startsWith(bufferCategoryDeviceList.get(i))){ //перевели всю строку запроса в нижний регистр
+
+                String separator = File.separator;
+                Set<Path> resultDeviceInfoList = new TreeSet<>();
+                resultDeviceInfoList = new Device().findInfo(message,
+                        "src" + separator + "main" + separator + "resources" + separator + "dataBaseProducts");
+
+                if (!resultDeviceInfoList.isEmpty()) {
+                    for (Path resultDeviseInfo : resultDeviceInfoList) {
+                        SendPhoto replyPhoto = Response.createPhotoMessage(message,
+                                FilenameUtils.removeExtension(resultDeviseInfo.getFileName().toString())
+                                        + "\n\nТехнические характеристики и USP:\nhttp://uspmobile.ru/",
+                                resultDeviseInfo.getParent().resolve(resultDeviseInfo.getFileName()).toString());
+
+                        return replyPhoto;
+                    }
+                } else {
+                    var replyMessage = Response.createTextMessageWithKeyboard(message,
+                            "Устройство не найдено!\n\nСписок доступных для поиска устройств\n" +
+                                    "находится в разделе ИНФО стартового меню." + "\n\n" + "/start_menu"
+                            , Response.TypeKeyboard.START);
+                    return replyMessage;
+                }
+            }
+        }
+
+        if (messageText.equals(new BotData().getBotPassword()) || messageText.equals("/start_menu")) {
+
+            //Стартовое меню
+            var replyMessage = Response.createTextMessageWithKeyboard(message,
+                    "/start_menu", Response.TypeKeyboard.START);
+
+            return replyMessage;
+        }
+
+        if (messageText.equals("Инфо Мобайл")
+                || messageText.equals("Mobile info")
+                || messageText.equals("/mobile_info")) {
+
+            String heading = "Список доступных для поиска устройств:\n\nМобильная электроника:\n\n";
+            String separator = File.separator;
+            String directory = "src" + separator + "main" + separator + "resources" + separator + "dataBaseProducts" + separator + "mobile";
+            String ending = "\nСписок устройств в процессе пополнения...";
+            String replyTextMessage = new InfoCommand().create(heading, directory, ending);
+
+            var replyMessage = Response.createTextMessageWithKeyboard(message
+                    , replyTextMessage + "\n\n" + "/start_menu"
+                    , Response.TypeKeyboard.START);
+
+            return replyMessage;
+        }
+
+        if (messageText.equals("Инфо ТВ")
+                || messageText.equals("TV info")
+                || messageText.equals("/tv_info")) {
+
+            String heading = "Список доступных для поиска устройств:\n\nТелевизоры:\n\n";
+            String separator = File.separator;
+            String directory = "src" + separator + "main" + separator + "resources" + separator + "dataBaseProducts" + separator + "tv";
+            String ending = "\nСписок устройств в процессе пополнения...";
+            String replyTextMessage = new InfoCommand().create(heading, directory, ending);
+
+            var replyMessage = Response.createTextMessageWithKeyboard(message
+                    , replyTextMessage + "\n\n" + "/start_menu"
+                    , Response.TypeKeyboard.START);
+
+            return replyMessage;
+        }
+
+        if (messageText.equals("Инфо БТ")
+                || messageText.equals("Appliances info")
+                || messageText.equals("/appliances_info")) {
+
+            String heading = "Список доступных для поиска устройств:\n\nБытовая техника:\n\n";
+            String separator = File.separator;
+            String directory = "src" + separator + "main" + separator + "resources" + separator + "dataBaseProducts" + separator + "appliances";
+            String ending = "\nСписок устройств в процессе пополнения...";
+            String replyTextMessage = new InfoCommand().create(heading, directory, ending);
+
+            var replyMessage = Response.createTextMessageWithKeyboard(message
+                    , replyTextMessage + "\n\n" + "/start_menu"
+                    , Response.TypeKeyboard.START);
+
+            return replyMessage;
+        }
+
+        if (messageText.equals("Сервис")
+                || messageText.equals("Service")
+                || messageText.equals("/service")) {
+
+            var replyMessage = Response.createTextMessageWithKeyboard(message
+                    , new ServiceInfoCommand().create()+ "\n\n" + "/start_menu"
+                    ,Response.TypeKeyboard.START);
+
+            return replyMessage;
+        }
+
+        if (messageText.equals("Программа Лояльности")
+                || messageText.equals("/bonus_card")) {
+
+            var replyMessage = Response.createTextMessage(message,
+                    "Введите номер бонусной карты\n\n" +
+                            "Пример: 2000872486\n\n" +
+                            "или\n\n" +
+                            "Номер телефона Клиента\n\n" +
+                            "Пример: 9167303030");
+
+            return replyMessage;
+        }
+
+        if (messageText.startsWith("9")
+                || messageText.startsWith("200")) {
+
+            var replyMessage = Response.createTextMessageWithKeyboard(message
+                    , "Поиск пока в разработке..." + "\n\n" + "/start_menu"
+                    ,Response.TypeKeyboard.START);
+
+            return replyMessage;
+        }
+
+        //Стартовое меню
+        var replyMessage = Response.createTextMessageWithKeyboard(message
+                ,"Команда не найдена!" + "\n\n" + "/start_menu"
+                ,Response.TypeKeyboard.START);
+
+        return replyMessage;
     }
-
 }
