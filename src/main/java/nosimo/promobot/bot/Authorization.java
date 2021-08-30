4b40f99e522473eca7308ef630bfd3da31fa904e
chilikinow <Sam9858853565>
2021-08-30 10:33:58 +0300

    1.5 create authorization with telegram UserName

--- src/main/java/nosimo/promobot/bot/Authorization.java
+++ src/main/java/nosimo/promobot/bot/Authorization.java
@@ -2,16 +2,22 @@ package nosimo.promobot.bot;
 
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Authorization {
 
     public boolean pass(String enterUserName){
+
+        enterUserName = "@" + enterUserName;
 
         Path userNameFile = Paths.get(".")
                 .toAbsolutePath()
@@ -30,6 +36,19 @@ public class Authorization {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        userNameList = userNameList.stream()
+                .filter(u -> !u.equals("")
+                        && !u.isEmpty()
+                        && !u.startsWith("+")
+                        && !u.startsWith("\"")
+                        && !Pattern.matches(".*\\p{InCyrillic}.*", u))
+                .map(u -> {
+                    if (!u.startsWith("@"))
+                        u = "@" + u;
+                    return u;
+                })
+                .collect(Collectors.toList());
 
         if (userNameList.contains(enterUserName)) {
             return true;
