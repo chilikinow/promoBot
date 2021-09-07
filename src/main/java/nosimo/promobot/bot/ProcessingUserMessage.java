5c83cc41f6d0ab2b838d257b8e816c8aafece4c8
chilikinow <Sam9858853565>
2021-09-07 23:28:41 +0300

    1.5 optimization code

--- src/main/java/nosimo/promobot/bot/ProcessingUserMessage.java
+++ src/main/java/nosimo/promobot/bot/ProcessingUserMessage.java
@@ -14,6 +14,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class ProcessingUserMessage {
 
@@ -79,7 +80,7 @@ public class ProcessingUserMessage {
                     .replaceAll("plus", "\\+")
                     .replace("+", "\\+");
 
-            if (deviceMessageText.startsWith(bufferCategoryDeviceList.get(i))){ //перевели всю строку запроса в нижний регистр
+            if (deviceMessageText.startsWith(bufferCategoryDeviceList.get(i)) && !Pattern.matches(".*\\p{InCyrillic}.*", deviceMessageText)){
 
                 if (deviceMessageText.length() == 1){
                     SendMessage replyMessage = Response.createTextMessage(chatId
