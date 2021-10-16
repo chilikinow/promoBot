14e5ae4f7032cf54567c04e34f196c23e55471e2
chilikinow <Sam9858853565>
2021-10-16 21:15:14 +0300

    1.5 change tests

--- src/main/java/nosimo/promobot/bot/authorization/AuthorizationWithUsername.java
+++ src/main/java/nosimo/promobot/bot/authorization/AuthorizationWithUsername.java
@@ -11,24 +11,43 @@ import java.util.stream.Collectors;
 
 public class AuthorizationWithUsername {
 
+    private static List<String> usernamelist;
+
+    static {
+        usernamelist = new ArrayList<>();
+    }
+
+    public static void setUsernameToList(String ...usernames){
+        if (usernames.length > 0)
+            for (String username: usernames)
+                usernamelist.add(username);
+    }
+
+    public static void removeUsernameToList(String ...usernames){
+        if (usernames.length > 0)
+            for (String username: usernames)
+                if (usernamelist.contains(username))
+                    usernamelist.remove(username);
+    }
+
     public static boolean pass(String enterUserName){
 
         enterUserName = "@" + enterUserName;
 
-        List<String> userNameList = new ArrayList<>();
-
         Path userNameFile = BotData.outResources.resolve("users.txt");
 
         try {
-            userNameList = new ArrayList<>(Files.readAllLines(userNameFile));
+            usernamelist.addAll(Files.readAllLines(userNameFile));
         } catch (IOException e) {
             System.out.println("File users.txt not found...");
         }
 
-        userNameList = userNameList.stream()
+        usernamelist = usernamelist.stream()
                         .filter(u -> !u.equals("")
                                 && !u.startsWith("+")
                                 && !u.startsWith("\"")
+                                && !u.startsWith("\\")
+                                && !u.startsWith(" ")
                                 && !Pattern.matches(".*\\p{InCyrillic}.*", u))
                         .map(u -> {
                             if (!u.startsWith("@"))
@@ -37,11 +56,11 @@ public class AuthorizationWithUsername {
                         })
                         .collect(Collectors.toList());
 
-        if (userNameList.isEmpty()) {
+        if (usernamelist.isEmpty()) {
             return true;
         }
 
-        if (userNameList.contains(enterUserName)) {
+        if (usernamelist.contains(enterUserName)) {
             return true;
         }
 
--- src/test/java/nosimo/promobot/bot/authorization/AuthorizationWithUsernameTest.java
+++ src/test/java/nosimo/promobot/bot/authorization/AuthorizationWithUsernameTest.java
@@ -1,14 +1,91 @@
 package nosimo.promobot.bot.authorization;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuthorizationWithUsernameTest {
 
-    @Test
-    void usernameShouldBeNotPass(){
-        boolean result = AuthorizationWithUsername.pass("test_not_pass_username");
+    @BeforeAll
+    static void initTestArtefacts(){
+        AuthorizationWithUsername.setUsernameToList(
+                "test_pass_username",
+                "testPassUsername",
+                "Test.pass.username1",
+                "@test_pass_username",
+                "@testPassUsername",
+                "@Test.pass.username1",
+                "",
+                " ",
+                "+79999999999",
+                "\"",
+                "\\",
+                "уборщица",
+                "уволен"
+                );
+    }
+
+    @DisplayName("Username should be pass")
+    @ParameterizedTest
+    @MethodSource("nosimo.promobot.bot.authorization.AuthorizationWithUsernameTest" +
+            "#getGetArgumentsForPassAuthorizationWithUsernameTest")
+    void usernameShouldBePass(String username){
+        boolean result = AuthorizationWithUsername.pass(username);
+        assertTrue(result);
+    }
+
+    @DisplayName("Arguments for pass authorization with username")
+    static Stream<Arguments> getGetArgumentsForPassAuthorizationWithUsernameTest(){
+        return Stream.of(
+                Arguments.of("test_pass_username"),
+                Arguments.of("testPassUsername"),
+                Arguments.of("Test.pass.username1")
+        );
+    }
+
+    @DisplayName("Username should be not pass")
+    @ParameterizedTest
+    @MethodSource("nosimo.promobot.bot.authorization.AuthorizationWithUsernameTest" +
+            "#getGetArgumentsForNotPassAuthorizationWithUsernameTest")
+    void usernameShouldBeNotPass(String username){
+        boolean result = AuthorizationWithUsername.pass(username);
         assertFalse(result);
+    }
+
+    @DisplayName("Arguments for not pass authorization with username")
+    static Stream<Arguments> getGetArgumentsForNotPassAuthorizationWithUsernameTest(){
+        return Stream.of(
+                Arguments.of("test_not_pass_username"),
+                Arguments.of(""),
+                Arguments.of(" "),
+                Arguments.of("+79999999999"),
+                Arguments.of("уборщица"),
+                Arguments.of("\""),
+                Arguments.of("уволен")
+                );
+    }
+
+    @AfterAll
+    static void removeTestArtefacts(){
+        AuthorizationWithUsername.removeUsernameToList(
+                "test_pass_username",
+                "testPassUsername",
+                "Test.pass.username1",
+                "@test_pass_username",
+                "@testPassUsername",
+                "@Test.pass.username1",
+                "",
+                " ",
+                "+79999999999",
+                "\"",
+                "\\",
+                "уборщица",
+                "уволен"
+        );
     }
 }
